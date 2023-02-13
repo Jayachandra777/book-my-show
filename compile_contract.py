bcfe4a11943de8ee60e30eacf1bccd229db9fff9
Jayachandra777 <124385565+Jayachandra777@users.noreply.github.com>
2023-02-14 00:23:10 +0530

    Update compile_contract.py

--- compile_contract.py
+++ compile_contract.py
@@ -1,21 +1,48 @@
-from pyteal import * 
-  
-from Bookmyshow import Bookmyshow 
-  
-if __name__ == "__main__": 
-     approval_program = Bookmyshow().approval_program() 
-     clear_program = Bookmyshow().clear_program() 
-  
-     # Mode.Application specifies that this is a smart contract 
-     compiled_approval = compileTeal(approval_program, Mode.Application, version=6) 
-     print(compiled_approval) 
-     with open("Bookmyshow_approval.teal", "w") as teal: 
-         teal.write(compiled_approval) 
-         teal.close() 
-  
-     # Mode.Application specifies that this is a smart contract 
-     compiled_clear = compileTeal(clear_program, Mode.Application, version=6) 
-     print(compiled_clear) 
-     with open("Bookmyshow_clear.teal", "w") as teal: 
-         teal.write(compiled_clear) 
-         teal.close()
+from pyteal import *
+
+class Bookmyshow:
+
+    def approval_program(self):
+
+        # Define your TEAL approval program here
+
+        # For example:
+
+        program = Return(Int(1))
+
+        return program
+
+    def clear_program(self):
+
+        # Define your TEAL clear program here
+
+        # For example:
+
+        program = Return(Int(1))
+
+        return program
+
+if __name__ == "__main__":
+
+    bms = Bookmyshow()
+
+    approval_program = bms.approval_program()
+
+    clear_program = bms.clear_program()
+
+    # Mode.Application specifies that this is a smart contract
+
+    compiled_approval = compileTeal(approval_program, Mode.Application, version=6)
+
+    with open("Bookmyshow_approval.teal", "w") as teal:
+
+        teal.write(compiled_approval)
+
+    # Mode.Application specifies that this is a smart contract
+
+    compiled_clear = compileTeal(clear_program, Mode.Application, version=6)
+
+    with open("Bookmyshow_clear.teal", "w") as teal:
+
+        teal.write(compiled_clear)
+
