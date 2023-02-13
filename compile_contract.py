3bcb7d4857f1b88b074ead5bcac73c7c0c8b33ca
Jayachandra777 <124385565+Jayachandra777@users.noreply.github.com>
2023-02-13 20:52:08 +0530

    Update compile_contract.py

--- compile_contract.py
+++ compile_contract.py
@@ -1,33 +1,21 @@
-from pyteal import *
-
-from marketplace_contract import Product
-
-if __name__ == "__main__":
-
-    approval_program = Product().approval_program()
-
-    clear_program = Product().clear_program()
-
-    # Mode.Application specifies that this is a smart contract
-
-    compiled_approval = compileTeal(approval_program, Mode.Application, version=6)
-
-    print(compiled_approval)
-
-    with open("marketplace_approval.teal", "w") as teal:
-
-        teal.write(compiled_approval)
-
-        teal.close()
-
-    # Mode.Application specifies that this is a smart contract
-
-    compiled_clear = compileTeal(clear_program, Mode.Application, version=6)
-
-    print(compiled_clear)
-
-    with open("marketplace_clear.teal", "w") as teal:
-
-        teal.write(compiled_clear)
-
-        teal.close()
+from pyteal import * 
+  
+ from Bookmyshow import Bookmyshow 
+  
+ if __name__ == "__main__": 
+     approval_program = Bookmyshow().approval_program() 
+     clear_program = Bookmyshow().clear_program() 
+  
+     # Mode.Application specifies that this is a smart contract 
+     compiled_approval = compileTeal(approval_program, Mode.Application, version=6) 
+     print(compiled_approval) 
+     with open("Bookmyshow_approval.teal", "w") as teal: 
+         teal.write(compiled_approval) 
+         teal.close() 
+  
+     # Mode.Application specifies that this is a smart contract 
+     compiled_clear = compileTeal(clear_program, Mode.Application, version=6) 
+     print(compiled_clear) 
+     with open("Bookmyshow_clear.teal", "w") as teal: 
+         teal.write(compiled_clear) 
+         teal.close()
